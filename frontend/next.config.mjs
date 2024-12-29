64438287678aaf5b09cad89a4f4a84d5d785d3ae
VA_24 <24agnihotriv@smtexas.org>
2024-12-28 18:00:45 -0600

    backend path and vercel update

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
       return [
         {
           source: '/api/:path*',
-          destination: 'http://localhost:3001/api/:path*'
+          destination: 'https://zetamaxx-server.vercel.app/api/:path*'
         }
       ]
     }
